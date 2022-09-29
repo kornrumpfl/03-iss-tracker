8568a3910df69c8d27c3540169d573025747bdf7
Luiz Kornrumpf <luiz.kornrumpf@gmail.com>
2022-09-29 16:59:14 +0200

    Solution

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import Controls from './components/Controls'
+import Map from './components/Map'
+import './styles.css'
+
+const URL = 'https://api.wheretheiss.at/v1/satellites/25544'
+
+export default function App() {
+  const [coords, setCoords] = useState({
+    longitude: 0,
+    latitude: 0,
+  })
+
+useEffect(()=>{
+const interval = setInterval(getISSCoords(),5000);
+return()=>clearInterval(interval)
+},[])
+
+  async function getISSCoords() {
+      fetch(URL)
+    .then((response)=>response.json())
+    .then((data)=>setCoords({longitude:data.longitude, latitude:data.latitude}));
+    
+  }
+
+  return (
+    <main>
+      <Map {...coords} />
+      <Controls
+        longitude={coords.longitude}
+        latitude={coords.latitude}
+        onRefresh={getISSCoords}
+      />
+    </main>
+  )
+}
